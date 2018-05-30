07aba142e702524ca2a716aec8076ee27d56db31
Igor Yermak <igor.yermak@gmail.com>
2018-05-30 13:07:11 -0400

    Posting notes as logged in user or anon user

--- bookmarks/notes/api.py
+++ bookmarks/notes/api.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers, viewsets
 from .models import Note
 
+
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
-  def create(self, validated_data):
-      user: self.context['request'].user
+    def create(self, validated_data):
+        user = self.context['request'].user
 
-      note = Note.object.create(**validated_data)
-      return note
+        note = Note.objects.create(user=user, **validated_data)
+        return note
 
-  class Meta: 
-    model = Note
-    fields = ('title', 'content')
+    class Meta:
+        model = Note
+        fields = ('title', 'content')
+
 
 class NoteViewSet(viewsets.ModelViewSet):
-  serializer_class = NoteSerializer
-  queryset = Note.objects.all()  
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+
+    def get_queryset(self):
+      user = self.request.user
+      
+      if user.is_anonymous:
+          return Note.objects.none()
+      else:
+          return Note.objects.filter(user=user)
