98926874fee038807159cbb7d01c2f237e93e725
Igor Yermak <igor.yermak@gmail.com>
2018-06-06 01:23:35 -0400

    Mutation

--- bookmarks/notes/schema.py
+++ bookmarks/notes/schema.py
@@ -12,6 +12,7 @@ class Note(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
+    note = graphene.Field(Note, id=graphene.String(), title=graphene.String())
     all_notes = graphene.List(Note)
 
     def resolve_notes(self, info):
@@ -22,8 +23,44 @@ class Query(graphene.ObjectType):
         if settings.DEBUG:
             return NoteModel.objects.all()
         elif user.is_anonymous:
-          return NoteModel.objects.none()
+            return NoteModel.objects.none()
         else:
-          return NoteModel.objects.filter(user=user)
+            return NoteModel.objects.filter(user=user)
+
+    def resolve_note(self, info, **kwargs):
+        title = kwargs.get('title')
+        id = kwargs.get('id')
+
+        if title is not None:
+            return NoteModel.objects.get(title=title)
+
+        return None
+
+
+class CreateNote(graphene.Mutatation):
+
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    ok = graphene.Boolean()
+    note = graphene.Field(Note)
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            is_ok = False
+            return CreateNote(ok=is_ok)
+
+        else: 
+            new_note = NoteModel(title = title, content = content, user = user)
+            new_note.save()
+            is_ok = True
+
+            return CreateNote(note=new_note, ok=is_ok)
+
+class Mutation(graphene.ObjectType):
+    create_note = CreateNote.Field()
 
 schema = graphene.Schema(query=Query)
